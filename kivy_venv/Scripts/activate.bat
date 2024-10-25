948842faadfebd6e89ff8d75a3b78d30997f3bbf
Ali <54626264+Ali-khajavi@users.noreply.github.com>
2024-10-25 08:16:08 +0200

    first commit

--- kivy_venv/Scripts/activate.bat
+++ kivy_venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Ali\Documents\GitHub\Monitoring-kivy\kivy_venv
+set VIRTUAL_ENV=C:\Users\Monitoring\OneDrive - Pillipp Haustechnik GmbH\Dokumente\GitHub\Monitoring-kivy\kivy_venv
 
 if not defined PROMPT set PROMPT=$P$G
 
