0bb2b039b2a4ef7826bffef1521d087b94fab6f5
Ali <54626264+Ali-khajavi@users.noreply.github.com>
2024-11-19 12:32:50 +0100

    Update first_menu_screen.py

--- first_menu_screen.py
+++ first_menu_screen.py
@@ -1,4 +1,22 @@
 from kivy.uix.screenmanager import Screen
+from kivy.properties import StringProperty
+import os, sys
+
+def resource_path(relative_path):
+        """Get absolute path to resource, works for dev and PyInstaller"""
+        try:
+            # PyInstaller temporary folder
+            base_path = sys._MEIPASS
+        except AttributeError:
+            # Development environment
+            base_path = os.path.abspath(".")
+        return os.path.join(base_path, relative_path)
 
 class FirstMenuScreen(Screen):
+    background_image = StringProperty(resource_path('assets/Backgroundimage.png'))
+    button_b3 = StringProperty(resource_path('assets/PNG/Button_1/b3.png'))
+    button_b4 = StringProperty(resource_path('assets/PNG/Button_1/b4.png'))
+    button_b1 = StringProperty(resource_path('assets/PNG/Button_1/b1.png'))
+    button_b2 = StringProperty(resource_path('assets/PNG/Button_1/b2.png'))
+
     pass
